e1fcea5fd78e5d99b4762b1b720eba577cf96afd
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-11-11 15:17:52 +0100

    Fix some issues on the file of task0 'alx_book_store.sql'

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    auhtor_id INT PRIMARY KEY,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
@@ -12,7 +12,7 @@ CREATE TABLE Books (
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(auhtor_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers (
@@ -24,15 +24,15 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    customer_id FOREIGN KEY,
+    customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid PRIMARY KEY,
-    order_id FOREIGN KEY,
-    book_id FOREIGN KEY,
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
